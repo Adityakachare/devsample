ff95c96dd30ccb914f31dde5ba44e39872a19262
Adityakachare <aadityakachare132@gmail.com>
2023-07-20 22:57:16 +0530

    Day12 JS

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("This is the js code from the html file");
+const myName = "Aditya";
+const year = "2003";
+console.log(myName);
+console.log(year);
+
+var num = [1,2,4,5,"Aditya"];
+console.log(num);
